237182caeb779de2b6ef877dc2fff3bc25ae01b9
1164676 <1164676>
2020-03-12 15:31:05 -0300

    Cinema

---
+++ Lista11/Exercicio05/Cinema.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05
+{
+    class Cinema
+    {
+        private DateTime data;
+        public Cinema(DateTime data) {
+            this.data = data;
+        }
+        public void SetDia(DateTime data) {
+            this.data = data;
+        }
+        public double GetInteira() {
+            double p = 20;
+            switch(data.DayOfWeek) {
+                case DayOfWeek.Wednesday: return 8;
+                case DayOfWeek.Monday :
+                case DayOfWeek.Tuesday :
+                case DayOfWeek.Thursday : p = 16; break;
+            }
+            if (data.Hour >= 17) p *= 1.5;
+            return p;
+        }
+        public double GetMeiaEntrada() {
+            if (data.DayOfWeek == DayOfWeek.Wednesday) return 8;
+            return GetInteira() / 2;
+        }
+        public override string ToString() {
+            return $"{data:dddd} {data:HH:mm}";
+        }
+    }
+}
---
+++ Lista11/Exercicio05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZoneInfo localZone = TimeZoneInfo.Local;
+            Console.WriteLine("Local Time Zone ID: {0}", localZone.Id);
+            Console.WriteLine("   Display Name is: {0}.", localZone.DisplayName);
+            Console.WriteLine("   Standard name is: {0}.", localZone.StandardName);
+            Console.WriteLine("   Daylight saving name is: {0}.", localZone.DaylightName);
+            //Console.WriteLine("Informe a data no formato dd/mm/aaaa hh:mm");
+            //Cinema x = new Cinema(DateTime.Parse(Console.ReadLine()));
+            Cinema x = new Cinema(DateTime.Now);
+            Console.WriteLine(x);
+            Console.WriteLine(x.GetInteira());
+            Console.ReadKey();
+        }
+    }
+}
