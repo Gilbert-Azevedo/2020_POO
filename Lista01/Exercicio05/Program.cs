3c82d3a715693f1556ceb500a998e2dc3ba48d29
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-04-09 15:34:06 -0300

    Solução completa da lista 01

--- Lista01/Exercicio05/Program.cs
+++ Lista01/Exercicio05/Program.cs
@@ -10,24 +10,27 @@ namespace Exercicio05
     {
         static void Main(string[] args)
         {
+            // O método Split é usado para dividir de acordo com um separador
+            // No exemplo, o separador é o caractere '/' da data
+            // O resultado do Split é um vetor de elementos em que cada
+            // elemento é acessado com um índice que inicia em zero
+            // O método Substring é usado para recuperar parte de um texto
+
+            // Mais sobre Split em: https://docs.microsoft.com/pt-br/dotnet/api/system.string.split?view=netframework-4.8
+            // Mais sobre Substring em: https://docs.microsoft.com/pt-br/dotnet/api/system.string.substring?view=netframework-4.8
+
             Console.WriteLine("Digite sua data de nascimento:");
             string s = Console.ReadLine();
             string[] x = s.Split('/');
-            int a = int.Parse(x[2]);
-            int ano = int.Parse(s.Substring(6));
-            int mes = int.Parse(s.Substring(3, 2));
-            int dia = int.Parse(s.Substring(0, 2));
+            // O terceiro elemento possui o valor do ano necessário para o cálculo do exercício  
+            // int d = int.Parse(x[0]); // dia - não necessário no exemplo
+            // int m = int.Parse(x[1]); // mês - não necessário no exemplo
+            int a = int.Parse(x[2]); // ano
+            // Como recuperar dia, mês e ano com Substring
+            // int d = int.Parse(s.Substring(0, 2));
+            // int m = int.Parse(s.Substring(3, 2));
+            // int a = int.Parse(s.Substring(6));
             Console.WriteLine($"Em 31/12/2020 você terá {2020 - a} anos");
-
-            DateTime d = DateTime.Parse(s);
-            Console.WriteLine($"Em 31/12/2020 você terá {2020 - d.Year} anos");
-            Console.WriteLine(d.ToString("dddd"));
-            Console.WriteLine(d.ToString("d/MM/yyyy"));
-            Console.WriteLine(d.ToString("dd/MMM/yyyy"));
-            Console.WriteLine(d.ToString("ddd/MMMM/yyyy"));
-            Console.WriteLine(d.DayOfYear);
-            Console.WriteLine(d.AddDays(1));
-            Console.WriteLine(d.AddMonths(1));
             Console.ReadKey();
         }
     }
