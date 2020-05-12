0e6a33c1b0046372d5e920d0494cfed68fe2691d
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-05-12 17:35:26 -0300

    Solução parcial da lista 09: Exercícios 01 a 13

--- Lista09/Exercicio07/Program.cs
+++ Lista09/Exercicio07/Program.cs
@@ -8,19 +8,28 @@ namespace Exercicio07
 {
     class Program
     {
-        public static void AntecessorSucessor(int x,
-            out int antecessor, out int sucessor)
-        {
-            antecessor = x - 1;
-            sucessor = x + 1;
-        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Informe um valor inteiro");
+            int n = int.Parse(Console.ReadLine());
+            // O método requer a passagem de duas variáveis para os parâmetros de saída
+            // Essas variáveis precisam ser declaradas e vão retornar com os 
+            // valores atribuídos dentro do método
             int a, b;
-            AntecessorSucessor(5, out a, out b);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+            // A palavra reservada out deve ser utilizada também na chamada do método
+            AntecessorSucessor(n, out a, out b);
+            Console.WriteLine($"Valor informado = {n}");
+            Console.WriteLine($"Antecessor = {a}");
+            Console.WriteLine($"Sucessor   = {b}");
             Console.ReadKey();
+        }
+        public static void AntecessorSucessor(int x, out int antecessor, out int sucessor)
+        {
+            // Neste exemplo, o método retorna os valores nos parâmetros de saída
+            // sinalizados com out
+            // Não é neessário usar o return em métodos void
+            antecessor = x - 1;
+            sucessor = x + 1;
         }
     }
 }
--- Lista09/Exercicio13/Program.cs
+++ Lista09/Exercicio13/Program.cs
@@ -8,21 +8,26 @@ namespace Exercicio13
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um texto com vários espaço desnecessários");
+            string s = Console.ReadLine();
+            // Chama o método que remove os espaços e mostra o resultado
+            Console.WriteLine(RemoverEspacos(s));
+            Console.ReadKey();
+        }
         public static string RemoverEspacos(string texto)
         {
+            // Remove espaços no início e final do texto
             texto = texto.Trim();
-            string[] s = texto.Split(' ');
+            // Divide o texto em palavras
+            string[] s = texto.Split();
+            // Inicia o resultado com um texto vazio
             string r = string.Empty;
+            // Concatena todas as palavras não vazias encontradas
             foreach (string c in s)
                 if (c != "") r += (c + " ");
             return r;
-        }
-        static void Main(string[] args)
-        {
-            string s = "   William    Henry    Gates    III    ";
-            Console.WriteLine(RemoverEspacos(s));
-            Console.ReadKey();
-
         }
     }
 
