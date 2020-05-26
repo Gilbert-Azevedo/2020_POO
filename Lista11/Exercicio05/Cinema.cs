76d7d65b28c1c9053e97cdf6daae34dc5645f752
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-05-26 19:41:32 -0300

    Solução completa da lista 11

---
+++ Lista11/Exercicio05/Cinema.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05
+{
+    class Cinema
+    {
+        // Atributos - É possível atribuir valores iniciais para os parâmetros
+        // conforme mostrado abaixo
+        private string dia = "Dom";  
+        private int horario = 17;
+        // Métodos Acessores
+        public void SetDia(string dia)
+        {
+            // Valores válidos para dia são: Dom, Seg, Ter, Qua, Qui, Sex, Sab
+            if (dia == "Dom" || dia == "Seg" || dia == "Ter" || dia == "Qua" ||
+                dia == "Qui" || dia == "Sex" || dia == "Sab")
+                this.dia = dia;
+        }
+        public string GetDia()
+        {
+            return dia;
+        }
+        public void SetHorario(int horario)
+        {
+            // Valores válidos para horário são: 0 a 23
+            if (horario >= 0 && horario <= 23) this.horario = horario;
+        }
+        public int GetHorario()
+        {
+            return horario;
+        }
+        // Métodos de cálculo doe valor da entrada
+        public double Inteira()
+        {
+            // Verifica se o dia é quarta-feira
+            if (dia == "Qua") return 8;
+            // Dia é segunda, terça ou quinta
+            double p = 16;
+            // Verifica se o dia é sexta, sábado ou domingo
+            if (dia == "Sex" || dia == "Sab" || dia == "Dom") p = 20;
+            // Verifica os horários de acréscimo
+            if (horario == 0 || (horario >= 17 && horario <= 23)) p = p * 1.5;
+            return p;
+        }
+        public double Meia()
+        {
+            // Verifica se o dia é quarta-feira
+            if (dia == "Qua") return 8;
+            // Senão, o valor é metade da inteira
+            return Inteira() / 2;
+        }
+    }
+}
--- Lista11/Exercicio05/Program.cs
+++ Lista11/Exercicio05/Program.cs
@@ -10,6 +10,16 @@ namespace Exercicio05
     {
         static void Main(string[] args)
         {
+            Cinema c = new Cinema();
+            // Dados para cálculo da entrada
+            Console.WriteLine("Informe o dia do cimema: Dom, Seg, Ter, Qua, Qui, Sex ou Sab");
+            c.SetDia(Console.ReadLine());
+            Console.WriteLine("Informe o horário");
+            c.SetHorario(int.Parse(Console.ReadLine()));
+            // Resultado
+            Console.WriteLine($"Dia = {c.GetDia()} - Horário = {c.GetHorario()}");
+            Console.WriteLine($"Inteira = {c.Inteira():c2} - Meia = {c.Meia():c2}");
+            Console.ReadKey();
         }
     }
 }
